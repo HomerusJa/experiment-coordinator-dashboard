7c3bbb793bc91e3190c6ecb635c442f849b61728
Jakob Schluse <jakob@schluse.com>
2024-10-21 12:09:06 +0200

    Finish broker.py

--- server_code/s3i/broker.py
+++ server_code/s3i/broker.py
@@ -1,11 +1,8 @@
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-import anvil.secrets
-import anvil.server
+from dataclasses import dataclass
+from typing import Optional
 
-from loguru import logger
 import httpx
+from loguru import logger
 
 from . import auth
 from . import exceptions
@@ -13,12 +10,44 @@ from . import exceptions
 DEFAULT_BROKER_URL = "https://broker.s3i.vswf.dev"
 
 
+@dataclass
+class Thing:
+    id: str
+    secret: str
+
+    message_queue: Optional[str] = None
+    event_queue: Optional[str] = None
+
+    def __post_init__(self):
+        # Find default message and event queues
+        if self.message_queue is None:
+            self.message_queue = f"s3ibs://{self.id}"
+            logger.warning(
+                f"No message queue provided. Generated default message queue: {self.message_queue}"
+            )
+
+        if self.event_queue is None:
+            self.event_queue = f"s3ib://{self.id}/event"
+            logger.warning(
+                f"No event queue provided. Generated default event queue: {self.event_queue}"
+            )
+
+
 class Broker:
-    def __init__(self, id: str, secret: str, client: httpx.AsyncClient = None, broker_url: str = DEFAULT_BROKER_URL):
+    def __init__(
+        self,
+        self_thing: Thing,
+        client: httpx.AsyncClient = None,
+        broker_url: str = DEFAULT_BROKER_URL,
+    ):
         self.broker_url = broker_url
         self.client = client or httpx.AsyncClient()
         self.external_client = client is None
-        self.auth = auth.ClientAuthenticator(id, secret, self.client)
+        self.auth = auth.ClientAuthenticator(
+            self_thing.id, self_thing.secret, self.client
+        )
+        self.message_queue = self_thing.message_queue
+        self.event_queue = self_thing.event_queue
 
     def __del__(self):
         if not self.external_client:
@@ -34,27 +63,32 @@ class Broker:
         response = await self.client.post(url, headers=headers, json=message)
 
         if response.status_code != 201:
-            raise exceptions.S3IException(f"Failed to send message to {endpoint}.",
-                               headers=response.headers,
-                               body=message,
-                               status_code=response.status_code,
-                               response=response.text)
+            raise exceptions.S3IException(
+                f"Failed to send message to {endpoint}.",
+                headers=response.headers,
+                body=message,
+                status_code=response.status_code,
+                response=response.text,
+            )
         logger.success("Message sent successfully.")
 
     async def receive(self, event: bool = False, all: bool = False) -> dict:
         """Receive a message from the message broker."""
+        endpoint = self.event_queue if event else self.message_queue
         token = await self.auth.obtain_token()
         headers = token.header
-        url = f"{self.broker_url}/{endpoint}{'\event' if event else ''}{'\all' if all else ''}"
+        url = f"{self.broker_url}/{endpoint}{'\all' if all else ''}"
 
         logger.trace(f"Sending request to {url}.")
         response = await self.client.get(url, headers=headers)
 
         if response.status_code != 200:
-            raise exceptions.S3IException(f"Failed to get message from {endpoint}.",
-                               headers=response.headers,
-                               status_code=response.status_code,
-                               response=response.text)
+            raise exceptions.S3IException(
+                f"Failed to get message from {endpoint}.",
+                headers=response.headers,
+                status_code=response.status_code,
+                response=response.text,
+            )
         logger.success("Message received successfully.")
 
         return response.json()
