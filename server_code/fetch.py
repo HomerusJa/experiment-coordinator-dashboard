b1b7311466f6f907119178b5616863ad9b914e7b
Jakob Schluse <jakob@schluse.com>
2024-10-21 12:10:10 +0200

    Update fetch.py

--- server_code/fetch.py
+++ server_code/fetch.py
@@ -1,14 +1,19 @@
 import anvil.secrets
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-import anvil.server
+import httpx
 
 from .s3i import broker
 
-broker = broker.Broker()
+client = httpx.AsyncClient()
+self_thing = broker.Thing(
+    id=anvil.secrets.get_secret("s3i_id"),
+    secret=anvil.secrets.get_secret("s3i_secret"),
+    message_queue=anvil.secrets.get_secret("s3i_message_queue"),
+    event_queue=anvil.secrets.get_secret("s3i_event_queue"),
+)
+broker = broker.Broker(self_thing, client=client)
 
 
 @anvil.server.background_task
 def fetch_s3i():
-  pass
+    """Fetch messages from the S3I message broker a single time and do the appropriate tasks."""
+    # TODO: Implement this function.
